82fb350bb78778233d60ac758ecc239dcfc77261
Zumisha <zumi4@rambler.ru>
2020-05-28 16:32:42 +0300

    Добавлены конфигурации для тестирования СЛАУ

--- Main.cs
+++ Main.cs
@@ -1,21 +1,14 @@
 ﻿using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Auto_Statistic.Storage;
-using Microsoft.CSharp;
 
 namespace Auto_Statistic
 {
@@ -29,7 +22,7 @@ namespace Auto_Statistic
         public Main()
         {
             InitializeComponent();
-            DataBase.Create();
+            //DataBase.Create();
             windowVars = ReadSettingsFile(settingsPath);
             InitFields();
         }
